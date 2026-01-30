fe2eefa2db842c6a16f325b0b8a164dabb6fb24e
Charles Poussot-Vassal <charles.poussot@gmail.com>
2026-01-30 16:16:43 +0100

    complex pairing

--- +mlf/points_selection.m
+++ +mlf/points_selection.m
@@ -1,4 +1,8 @@
-function [P_c,P_r,W,V,tab] = points_selection(p_c,p_r,tab,ord,minimal)
+function [P_c,P_r,W,V,tab] = points_selection(p_c,p_r,tab,ord,minimal,CONJUGATE)
+
+if nargin < 6
+    CONJUGATE = false;
+end
 
 n       = length(p_c);
 ord     = ord+1;
@@ -7,25 +11,44 @@ W_str   = [];
 V_str   = [];
 for ii = 1:n
     ni(ii)      = length(p_c{ii});
-    idxp_c{ii}  = unique(floor(linspace(1,numel(p_c{ii}),ord(ii))));
+    if CONJUGATE && (ii == 1)
+        idxp_c{1}   = unique(floor(linspace(1,numel(p_c{1})-1,floor(ord(1))/2)));
+        idxp_c{1}   = floor(idxp_c{1}/2)*2+1;
+        idxp_c{1}   = sort([idxp_c{1} idxp_c{1}+1]);
+    else
+        idxp_c{ii}  = unique(floor(linspace(1,numel(p_c{ii}),ord(ii))));
+    end
     %idxp_c{ii}  = unique(floor([1 linspace(3,numel(p_c{ii}),ord(ii)-1)]));
     jj          = 1;
-    while length(idxp_c{ii}) < ord(ii)
-        idxp_c{ii}  = unique([idxp_c{ii} jj]);
-        jj          = jj + 1;
+    if ~CONJUGATE 
+        while length(idxp_c{ii}) < ord(ii)
+            idxp_c{ii}  = unique([idxp_c{ii} jj]);
+            jj          = jj + 1;
+        end
     end
     if minimal
-        idxp_r{ii}  = unique(floor(linspace(1,numel(p_r{ii}),ord(ii))));
+        if CONJUGATE && (ii == 1)
+            idxp_r{1}   = unique(floor(linspace(1,numel(p_r{1})-1,floor(ord(1))/2)));
+            idxp_r{1}   = floor(idxp_r{1}/2)*2+1;
+            idxp_r{1}   = sort([idxp_r{1} idxp_r{1}+1]);
+        else
+            idxp_r{ii}  = unique(floor(linspace(1,numel(p_r{ii}),ord(ii))));
+        end
         jj = 1;
-        while length(idxp_r{ii}) < ord(ii)
-            idxp_r{ii}  = unique([idxp_r{ii} jj]);
-            jj          = jj + 1;
+        if ~CONJUGATE 
+            while length(idxp_r{ii}) < ord(ii)
+                idxp_r{ii}  = unique([idxp_r{ii} jj]);
+                jj          = jj + 1;
+            end
         end
     else
         idxp_r{ii}  = 1:numel(p_r{ii});
     end
     p_c{ii} = p_c{ii}(idxp_c{ii});
     p_r{ii} = p_r{ii}(idxp_r{ii});
+    %p_c{1}.'
+    %p_r{1}.'
+    %pause
     % 
     ii_str  = num2str(ii);
     tab_str = [tab_str '[idxp_c{' ii_str  '} ni(' ii_str ')+idxp_r{' ii_str '}],'];
